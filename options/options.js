325765401490c939e073c324f4709a60e5703dc9
Crowdbotics <team@crowdbotics.com>
2024-05-16 01:05:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-waterfall-47977",
+  url: "https://curly-waterfall-47977.botics.co",
+  api: "https://curly-waterfall-47977.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
